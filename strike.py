726d4165e0ced3c025d59da893facd10a42450a9
David E. Lambert <david@davidelambert.com>
2022-04-06 18:01:39 -0400

    whitespace formatting

--- strike.py
+++ strike.py
@@ -11,20 +11,20 @@ class Strike(Thread):
     multipliers = (0.25, 0.375, 0.5, 0.75, 1.0, 1.5)
     kit = {fp.stem: sa.WaveObject.from_wave_file(str(fp))
            for fp in Path('./kit-default').iterdir()}
-    
+
     def __init__(self, bpm=80, rest_prob=0.1, kick=False):
         self.bpm = bpm
         self.rest_prob = rest_prob
         self.kick = kick
-         
+
         Thread.__init__(self)
         self.daemon = True
         self.start()
-        
+
     def get_durations(self):
         timebase = 60 / self.bpm
         return [timebase * m for m in self.multipliers]
-    
+
     def run(self):
         while True:
             duration = random.choice(self.get_durations())
@@ -40,15 +40,18 @@ class Strike(Thread):
                 pass
             time.sleep(duration)
 
+
 if __name__ == '__main__':
     bpm = float(sys.argv[1])
     rest_prob = float(sys.argv[2])
-    
+
     left = Strike(bpm, rest_prob)
     right = Strike(bpm, rest_prob)
     kick = Strike(bpm, rest_prob, kick=True)
-    left ; right ; kick
-    
+    left
+    right
+    kick
+
     playing = True
     try:
         while playing:
